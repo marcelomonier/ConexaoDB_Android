a1ce2ebb705f0f47a10256aea1bf909226af4608
marcelomonier <marcelomonier@gmail.com>
2018-09-12 21:39:36 -0300

    first commit

---
+++ app/src/main/java/br/com/marcelomonier/conexaobd/DataBaseHelper.java
@@ -0,0 +1,58 @@
+package br.com.marcelomonier.conexaobd;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+i
+public class DataBaseHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "Profissao.db";
+    public static final String TABLE_NAME = "Empregado";
+
+    public static final String COL_1 = "ID";
+    public static final String COL_2 = "NOME";
+    public static final String COL_3 = "SOBRENOME";
+    public static final String COL_4 = "PROFISSAO";
+
+
+    public DataBaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+        sqLiteDatabase.execSQL("CREATE TABLE " + TABLE_NAME + "(ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "NOME TEXT, SOBRENOME TEXT, PROFISSAO TEXT)");
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+
+    }
+
+    public boolean inserirDados (String nome, String sobrenome, String profissao){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+
+        contentValues.put(COL_2, nome);
+        contentValues.put(COL_3, sobrenome);
+        contentValues.put(COL_4, profissao);
+
+        long result = db.insert(TABLE_NAME, null, contentValues);
+        db.close();
+
+        if (result == 1){
+            return false;
+        } else {
+            return true;
+        }
+
+
+
+    }
+}
---
+++ app/src/main/java/br/com/marcelomonier/conexaobd/MainActivity.java
@@ -0,0 +1,57 @@
+package br.com.marcelomonier.conexaobd;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    DataBaseHelper myDb;
+
+
+    EditText edit_nome, edit_sobrenome, edit_profissao;
+    Button btn_enviar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myDb = new DataBaseHelper(this);
+
+        edit_nome = (EditText) findViewById(R.id.editText4);
+        edit_sobrenome = (EditText) findViewById(R.id.editText5);
+        edit_profissao = (EditText) findViewById(R.id.editText2);
+
+        btn_enviar = (Button) findViewById(R.id.button);
+
+        btn_enviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String name = edit_nome.getText().toString();
+                String surname = edit_sobrenome.getText().toString();
+                String profession = edit_profissao.getText().toString();
+
+
+
+                Boolean result =  myDb.inserirDados(name, surname, profession);
+
+                if (result == true){
+                    Toast.makeText(MainActivity.this, "Dados inseridos com sucesso", Toast.LENGTH_SHORT).show();
+                } else {
+
+                    Toast.makeText(MainActivity.this, "Erro ao inserir dados", Toast.LENGTH_SHORT).show();
+
+                }
+
+
+            }
+        });
+
+
+    }
+}
